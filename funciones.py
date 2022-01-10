46d49560b757f3c92d9e8c68c6704eec806940db
infra_gcb <cabigerman@gmail.com>
2022-01-09 21:32:42 -0300

    Agregue el bucle y el sleep de 3 segundos

--- funciones.py
+++ funciones.py
@@ -1,13 +1,16 @@
 import logging
+import time
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    filename='application.log',
-    level=logging.DEBUG,
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+for x in range(10):
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        filename='application.log',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-activo = 'GOOG'
-logging.debug(f"se esta comprando {activo}")
-logging.info(f"se esta comprando {activo}")
-logging.warning("Esto es un mensaje de prueba")
+    activo = 'GOOG'
+    logging.debug(f"se esta comprando {activo}")
+    logging.info(f"se esta comprando {activo}")
+    logging.warning("Esto es un mensaje de prueba")
+    time.sleep(3)
